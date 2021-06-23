a8c7036a4de4c94dad6bd0033fb3b75a5c71e406
Linda-vargas <linda.vargas@unmsm.edu.pe>
2021-06-22 20:24:27 -0500

    listar usuarios

--- src/main/java/pe/gob/unmsm/sgv/rest/UsuarioController.java
+++ src/main/java/pe/gob/unmsm/sgv/rest/UsuarioController.java
@@ -1,7 +1,10 @@
 package pe.gob.unmsm.sgv.rest;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +23,11 @@ public class UsuarioController {
 	    @PostMapping("/insertarusuario")
 	    public void save(@RequestBody Usuario u){
 	        us.añadirUsuario(u);
+	    }
+	    
+	    @GetMapping("/listar_usuarios")
+	    public List<Usuario> listar_usuarios(){
+	        return us.obtenerUsuarios();
 	    }
 	    
 	
